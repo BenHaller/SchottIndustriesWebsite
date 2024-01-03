6bed9af84976ab3d7b9f3e420b4cf75ca04b3290
Benjamin James Haller <bhaller@uwo.ca>
2024-01-03 13:52:09 -0500

    Navbar and card changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function updateContentPadding() {
     var carouselHeight = document.getElementById('homepageCarousel').offsetHeight;
 
     // Set the top padding of the content-section
-    document.getElementById('contentSection').style.paddingTop = (carouselHeight - 60) + 'px';
+    document.getElementById('contentSection').style.paddingTop = (carouselHeight + 40) + 'px';
 
     // Get the left padding of the cards
     var cardsLeftPadding = document.getElementById('residentialElectricalCard').cardsLeftPadding;
@@ -12,8 +12,13 @@ function updateContentPadding() {
     document.getElementById('sectorsTitle').style.paddingLeft = cardsLeftPadding + 'px';
 }
 
+// Set initial padding
+document.getElementById('contentSection').style.paddingTop = 540 + 'px';
+
 // Update padding on page load
 updateContentPadding();
+
+
 
 // Update padding when the window is resized
 window.addEventListener('resize', function () {
